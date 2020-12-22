9c9099c620ad751a22238e29ee4b6f5ed6d967a1
ViratGSingh <viratgsingh99@gmail.com>
2020-12-22 15:24:31 +0530

    Update main.js

--- static/scripts/autocomplete/main.js
+++ static/scripts/autocomplete/main.js
@@ -1,7 +1,7 @@
 
-const MongoClient = require(['mongodb']).MongoClient;
-const uri = "mongodb+srv://wooshir:vgs41999@items.uxp6f.mongodb.net/steam?retryWrites=true&w=majority";
-const client = MongoClient(uri, { useNewUrlParser: true });
+var MongoClient = require(['mongodb']).MongoClient;
+var uri = "mongodb+srv://wooshir:vgs41999@items.uxp6f.mongodb.net/steam?retryWrites=true&w=majority";
+var client = new MongoClient(uri, { native_parser: true });
 client.connect(err => {
   const collection = client.db("steam").collection("g_recom");
   // perform actions on the collection object
