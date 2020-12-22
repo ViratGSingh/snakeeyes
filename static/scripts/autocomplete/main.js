2a68582860ba764e8825f28ffb203e7601e31275
ViratGSingh <viratgsingh99@gmail.com>
2020-12-22 15:02:07 +0530

    Update main.js

--- static/scripts/autocomplete/main.js
+++ static/scripts/autocomplete/main.js
@@ -1,10 +1,9 @@
 
-var MongoClient = require(['mongodb']).MongoClient;
-
-var uri = "mongodb://wooshir:vgs41999@items-shard-00-00.uxp6f.mongodb.net:27017,items-shard-00-01.uxp6f.mongodb.net:27017,items-shard-00-02.uxp6f.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-wbruxm-shard-0&authSource=admin&retryWrites=true&w=majority";
-MongoClient.connect(uri, function(err, client) {
-  const collection = client.db("test").collection("devices");
+const MongoClient = require(['mongodb']).MongoClient;
+const uri = "mongodb+srv://wooshir:vgs41999@items.uxp6f.mongodb.net/steam?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("steam").collection("g_recom");
   // perform actions on the collection object
-  console.log("success")
   client.close();
 });
