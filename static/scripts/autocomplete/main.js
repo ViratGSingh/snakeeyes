0b33ecd7711b231c94d6a4d1d7dc8a4a42080da7
ViratGSingh <viratgsingh99@gmail.com>
2020-12-22 15:42:40 +0530

    Update main.js

--- static/scripts/autocomplete/main.js
+++ static/scripts/autocomplete/main.js
@@ -1,4 +1,4 @@
 
 var MongoClient = require(['mongodb']).MongoClient;
 var uri = "mongodb+srv://wooshir:vgs41999@items.uxp6f.mongodb.net/steam?retryWrites=true&w=majority";
-console.log(MongoClient)
+console.log(MongoClient);
