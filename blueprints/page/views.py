0a6d1b454c4e7260cb3131ef3813c7e4aadb7358
ViratGSingh <viratgsingh99@gmail.com>
2020-12-23 13:35:49 +0530

    Update views.py

--- blueprints/page/views.py
+++ blueprints/page/views.py
@@ -118,7 +118,7 @@ def search():
 
         except:
             pl.append("No info")               
-        detail.append(",".join(pl))
+        detail.append(pl)
         return render_template('page/details.html',detail=detail)
         
   
