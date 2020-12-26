0cc98726ef7f314cd34fe3fe7024122c59a4de35
ViratGSingh <viratgsingh99@gmail.com>
2020-12-26 17:26:01 +0530

    autocomplete

---
+++ static/scripts/autcomplete/main.js
@@ -0,0 +1,85 @@
+const { MongoClient, ObjectID } = require("mongodb");
+
+const Express = require("express");
+
+const Cors = require("cors");
+
+const BodyParser = require("body-parser");
+
+const { request } = require("express");
+
+const client = new MongoClient("mongodb+srv://wooshir:vgs41999@items.uxp6f.mongodb.net/test?retryWrites=true&w=majority",{useUnifiedTopology: true});
+
+const server = Express();
+
+server.use(BodyParser.json());
+
+server.use(BodyParser.urlencoded({ extended: true }));
+
+server.use(Cors());
+
+var collection;
+
+
+
+
+
+server.listen("3000", async () => {
+
+    try {
+
+        await client.connect( );
+
+        collection = client.db("steam").collection("g_recom");
+
+    } catch (e) {
+
+        console.error(e);
+
+    }
+
+});
+
+
+server.get("/search", async (request, response) => {
+
+    try {
+        
+        let result = await collection.aggregate([
+
+            {
+
+                "$search": {
+
+                    "autocomplete": {
+
+                        "query": `${request.query.query}`,
+
+                        "path": "Key",
+
+                        "fuzzy": {
+
+                            "maxEdits": 2,
+
+                            "prefixLength": 3
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        ]).toArray();
+
+        response.send(result);
+
+    } catch (e) {
+
+        response.status(500).send({ message: e.message });
+
+    }
+
+});
+
