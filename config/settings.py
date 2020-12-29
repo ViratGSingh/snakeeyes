cfd54ce6fe55941d091d276970bb3b183e6dee39
ViratGSingh <viratgsingh99@gmail.com>
2020-12-29 16:29:54 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,5 +4,5 @@ import os
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 MAIL_USERNAME='justlistenmusic99@gmail.com'
-DEBUG = True
+#DEBUG = True
 SECRET_KEY="a2df83f29994c53909bd65bc0f55c502f57ca305735ab1fe"
