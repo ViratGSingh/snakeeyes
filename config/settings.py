397a3161891d6188259e7bc9f0d6eab6fb2d4bb9
ViratGSingh <viratgsingh99@gmail.com>
2021-01-06 20:45:25 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,10 @@
 import os
+
+
+
 # SQLAlchemy.
-#DATABASE_URL= 'postgresql://postgres:vgs41999@localhost:5432/users'
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+DATABASE_URL= 'postgresql://postgres:vgs41999@localhost:5432/users'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 MAIL_USERNAME='justlistenmusic99@gmail.com'
 #DEBUG = True
