d6565da5815fa36ed452a331f12256fd70092814
ViratGSingh <viratgsingh99@gmail.com>
2021-01-08 00:42:58 +0530

    .

--- blueprints/page/views.py
+++ blueprints/page/views.py
@@ -8,91 +8,91 @@ from flask_login import current_user
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import linear_kernel
-import pandas as pd
+#import pandas as pd
 import numpy as np
 
-def pr(name,tags):
+# def pr(name,tags):
      
-        tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), stop_words='english')
-        tfidf_matrix = tf.fit_transform(tags) 
-        # http://stackoverflow.com/questions/16078015/
-        scores = zip(tf.get_feature_names(),
-                    np.asarray(tfidf_matrix.sum(axis=0)).ravel())
-        sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
-        l=[]
-        for item in sorted_scores[:10]:
-            l.append(item[0])
-        tags= ",".join(l)
-        client = pymongo.MongoClient("mongodb+srv://wooshir:vgs41999@items.uxp6f.mongodb.net/test?retryWrites=true&w=majority")
-        db = client["steam"]
-        df=db["games"]
-        item=df.find()
-        games=[]
-        content=[]
-        developer=[]
-        r_date=[]
-        tt=[]
-        iurl=[]
-        ifree=[]
-        rec_r=[]
-        all_r=[]
-        games.append(name)
-        content.append(tags)
-        developer.append("no info")
-        r_date.append("no info")
-        tt.append("no info")
-        iurl.append("no info")
-        ifree.append("no info")
-        rec_r.append("no info")
-        all_r.append("no info")
-        for data in item:
+#         tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), stop_words='english')
+#         tfidf_matrix = tf.fit_transform(tags) 
+#         # http://stackoverflow.com/questions/16078015/
+#         scores = zip(tf.get_feature_names(),
+#                     np.asarray(tfidf_matrix.sum(axis=0)).ravel())
+#         sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+#         l=[]
+#         for item in sorted_scores[:10]:
+#             l.append(item[0])
+#         tags= ",".join(l)
+#         client = pymongo.MongoClient("mongodb+srv://wooshir:vgs41999@items.uxp6f.mongodb.net/test?retryWrites=true&w=majority")
+#         db = client["steam"]
+#         df=db["games"]
+#         item=df.find()
+#         games=[]
+#         content=[]
+#         developer=[]
+#         r_date=[]
+#         tt=[]
+#         iurl=[]
+#         ifree=[]
+#         rec_r=[]
+#         all_r=[]
+#         games.append(name)
+#         content.append(tags)
+#         developer.append("no info")
+#         r_date.append("no info")
+#         tt.append("no info")
+#         iurl.append("no info")
+#         ifree.append("no info")
+#         rec_r.append("no info")
+#         all_r.append("no info")
+#         for data in item:
           
           
-            if int(data["all_p"]) >= 75:
+#             if int(data["all_p"]) >= 75:
                 
-                games.append(data["name"])
-                developer.append(data["developer"])
-                r_date.append(data["release_date"])
-                tt.append(data["top_tags"])
-                iurl.append(data["img_url"])
-                ifree.append(data["price"])
-                rec_r.append(data["recent_rating"])
-                all_r.append(data["all_rating"])
-                tags_list=data["tags"]
-                if tags_list:
-                  t=",".join(tags_list)
-                else:
-                  t="no info"
-                tags_str=t
-                content.append(tags_str)
+#                 games.append(data["name"])
+#                 developer.append(data["developer"])
+#                 r_date.append(data["release_date"])
+#                 tt.append(data["top_tags"])
+#                 iurl.append(data["img_url"])
+#                 ifree.append(data["price"])
+#                 rec_r.append(data["recent_rating"])
+#                 all_r.append(data["all_rating"])
+#                 tags_list=data["tags"]
+#                 if tags_list:
+#                   t=",".join(tags_list)
+#                 else:
+#                   t="no info"
+#                 tags_str=t
+#                 content.append(tags_str)
                 
-            else:
-                continue  
+#             else:
+#                 continue  
               
-        game_df=pd.DataFrame({"Game":games,
-                              "Content":content,
-                              "Developer":developer,
-                              "Release_Date":r_date,
-                              "Top_Tags":tt,
-                              "Image_url":iurl,
-                              "Is_Free":ifree,
-                              "Rec_r":rec_r,
-                              "All_r":all_r})
-        tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), stop_words='english')
-        tfidf_matrix = tf.fit_transform(game_df['Content'])
-        cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix) 
-        results = {}
+#         game_df=pd.DataFrame({"Game":games,
+#                               "Content":content,
+#                               "Developer":developer,
+#                               "Release_Date":r_date,
+#                               "Top_Tags":tt,
+#                               "Image_url":iurl,
+#                               "Is_Free":ifree,
+#                               "Rec_r":rec_r,
+#                               "All_r":all_r})
+#         tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), stop_words='english')
+#         tfidf_matrix = tf.fit_transform(game_df['Content'])
+#         cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix) 
+#         results = {}
 
-        for idx, row in game_df[:1].iterrows():
-            similar_indices = cosine_similarities[idx].argsort()[:-15:-1]
+#         for idx, row in game_df[:1].iterrows():
+#             similar_indices = cosine_similarities[idx].argsort()[:-15:-1]
             
             
-            similar_items = [(cosine_similarities[idx][i] ,game_df["Game"][i] ,game_df["Developer"][i],game_df["Release_Date"][i],game_df["Top_Tags"][i],game_df["Image_url"][i] ,game_df["Is_Free"][i],game_df["Rec_r"][i],game_df["All_r"][i]) for i in similar_indices]
+#             similar_items = [(cosine_similarities[idx][i] ,game_df["Game"][i] ,game_df["Developer"][i],game_df["Release_Date"][i],game_df["Top_Tags"][i],game_df["Image_url"][i] ,game_df["Is_Free"][i],game_df["Rec_r"][i],game_df["All_r"][i]) for i in similar_indices]
 
-            # First item is the item itself, so remove it.
-            # Each dictionary entry is like: [(1,2), (3,4)], with each tuple being (score, item_id)
-            results[row['Game']] = similar_items[1:]
-        return results[name]
+#             # First item is the item itself, so remove it.
+#             # Each dictionary entry is like: [(1,2), (3,4)], with each tuple being (score, item_id)
+#             results[row['Game']] = similar_items[1:]
+#         return results[name]
 
 
 client = pymongo.MongoClient("mongodb+srv://wooshir:vgs41999@items.uxp6f.mongodb.net/test?retryWrites=true&w=majority")
@@ -367,17 +367,17 @@ def search():
         detail.append(",".join(tpl))
         return render_template('page/details.html',detail=detail)
         
-@page.route('/discover',  methods=["GET","POST"])
-def discover(): 
-    if current_user.is_authenticated:
-        name=current_user.email
-        user=db.search.find_one({"user":current_user.email})
-        tags=user["tags"]
-        games=pr(name,tags)
-        return render_template('page/search.html', 
-                                games=games) 
-    else:
-        return render_template('page/home.html')                           
+# @page.route('/discover',  methods=["GET","POST"])
+# def discover(): 
+#     if current_user.is_authenticated:
+#         name=current_user.email
+#         user=db.search.find_one({"user":current_user.email})
+#         tags=user["tags"]
+#         games=pr(name,tags)
+#         return render_template('page/search.html', 
+#                                 games=games) 
+#     else:
+#         return render_template('page/home.html')                           
 
 
                 
